5d7c5a9a2b4f65b82c64d4d67cf364b4ad473d75
Burak Kumaş <brkkumas@gmail.com>
2024-08-11 02:46:12 +0300

    add site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://burak-kumas.github.io/",
+  site: "https://burak-kumas.github.io/Context-Root-Website-Astro/",
   base: "/",
   integrations: [tailwind({ applyBaseStyles: true })],
 });
