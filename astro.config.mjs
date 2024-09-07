73b8f0351c20415d4599f2589e41936f533bd284
Burak Kumaş <brkkumas@gmail.com>
2024-09-07 18:11:38 +0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
